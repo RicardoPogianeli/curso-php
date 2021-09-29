c935ad7d24bdd372e74784e023ce6577d29c93dd
RicardoPogianeli <ricardopogianeli91@gmail.com>
2021-09-29 11:42:06 -0300

    Andamento Curso

---
+++ basico/comentarios.php
@@ -0,0 +1,20 @@
+<div class = "titulo">Comentários PHP</div>
+
+<?php
+echo "Estou no PHP";
+//echo "Comentário";
+# echo "Comentário";
+# echo // "Comentário";
+// echo # "Comentário";
+
+//?>
+
+<p>Depois do primeiro bloco!</p>
+
+<?php
+/*
+    Linha 1 do comentário
+    linha 2 do comentário
+    linha 3 do comentário'
+*/
+?>
---
+++ tipos/int.php
@@ -0,0 +1,13 @@
+<div class="titulo">Tipo Inteiro</div>
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 017, '<br>'; // base octal = adicione o zero no inicio
+echo ob110001111001, '<br>'; // base binaria
+echo 0x117acf0; // base hexa-decimal
